1cfb4a6ce1e4e2209c542e2c3024428da78af324
Tim Freeman <d.timothy.freeman@gmail.com>
2017-08-29 09:04:45 -0400

    read

---
+++ index.js
@@ -0,0 +1,9 @@
+function map(collection, callback) {
+	const result = [];
+
+	for (let i = 0; i < collection.length; i++) {
+		const element = collection[i];
+		result.push(callback(element));
+	}
+	return result;
+}
